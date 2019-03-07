5482cf83dfd41e53f107f2b9d4c7b8fe43e0fa4f
Richard Dowland <rdowland@hotmail.co.uk>
2019-03-07 13:54:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
+function nowServing(katzDeliLine){
+  if(katzDeliLine>[]) {
+    return `Currently serving ${katzDeliLine.shift()}.`;
+  }
+  else {
+    return "There is nobody waiting to be served!";
+  }
+}
